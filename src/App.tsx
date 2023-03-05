657d5b9cc3a0e021568d1c088da5427fbd88cf9a
moshawa <moshawa0@gmail.com>
2023-03-05 17:09:05 -0500

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
 import "./App.css"
-import Blob from "./components/Blob"
 
 function App() {
 	return (
@@ -9,8 +6,6 @@ function App() {
 			<div className="img-wrapper">
 				<img src="./logo.svg " alt="logo" />
 			</div>
-			<div className="pattern top"></div>
-			<div className="pattern"></div>
 			<div className="h1-wrapper">
 				<h1>قريباً</h1>
 				<h1>Coming Soon</h1>
