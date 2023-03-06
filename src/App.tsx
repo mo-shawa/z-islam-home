58e9360198593d7bd204c602156a57ae10669b4a
moshawa <moshawa0@gmail.com>
2023-03-06 03:36:55 -0500

    add top bar and fix scaling

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import "./App.css"
 function App() {
 	return (
 		<div className="App">
+			<nav>
+				<ul>
+					<li>
+						<p>AL Mahatta, Army street, Amman, Jordan</p>
+					</li>
+					<li>
+						<p>المحطة- شارع الجيش-عمان الاردن</p>
+					</li>
+					<li>
+						<a href="tel:064922537">064922537</a>
+					</li>
+					<li>
+						<a href="tel:064922538">064922538</a>
+					</li>
+					<li>
+						<a href="tel:064922539">064922539</a>
+					</li>
+					<li>
+						<a href="tel:+962770011208">+962770011208</a>
+					</li>
+				</ul>
+			</nav>
 			<div className="img-wrapper">
 				<img src="./logo.svg " alt="logo" />
 			</div>
 			<div className="h1-wrapper">
-				<h1>قريباً</h1>
 				<h1>Coming Soon</h1>
+				<h1>قريباً</h1>
 			</div>
 		</div>
 	)
